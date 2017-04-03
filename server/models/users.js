11d7f27e4dc7b06169afef1eb63ba027e4a7cd09
Tyler Collins <tjscollins@gmail.com>
2017-04-03 20:03:46 +1000

    Tweak user mongoose schema to include array of photos w/ urls

--- server/models/users.js
+++ server/models/users.js
@@ -1,13 +1,29 @@
 'use strict';
 
 const mongoose = require('mongoose');
+const validator = require('validator');
 const Schema = mongoose.Schema;
 
 const User = new Schema({
   twitter: {
     id: String,
-    displayName: String,
+    displayName: String
   },
+  photos: [
+    {
+      url: {
+        type: String,
+        required: true,
+        validate: {
+          validator: validator.isURL
+        }
+      },
+      desc: {
+        type: String,
+        required: true,
+      }
+    }
+  ]
 });
 
 module.exports = mongoose.model('User', User);
