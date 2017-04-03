8bb471ab035911cb8ce3c0f3f182ce030bc6f6f9
Tyler Collins <tjscollins@gmail.com>
2017-04-03 20:08:01 +1000

    Tweaked mongoose models to allow simpler storage of photos and their metadata

---
+++ server/models/photos.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const validator = require('validator');
+const Schema = mongoose.Schema;
+
+const Photo = new Schema({
+  url: {
+    type: String,
+    required: true,
+    validate: {
+      validator: validator.isURL
+    }
+  },
+  desc: {
+    type: String,
+    required: true,
+  },
+  creator: {
+    type: Schema.Types.ObjectId,
+  },
+  likes: [Schema.Types.ObjectId],
+});
+
+module.exports = mongoose.model('Photo', Photo);
